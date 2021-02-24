ab42cbb4cf756c2c95a9307ae698b8a688787b86
Carlton <salesbycarlton@gmail.com>
2021-02-24 03:39:10 -0600

    cherno vertex buffers ep.4 complete

--- OpenGLProject/Application.cpp
+++ OpenGLProject/Application.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -19,11 +22,43 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* glewInit cannot be called before glfw context created */
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    /* get opengl version */
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float vertexPositions[6] = {
+        -0.5f,-0.5f,
+         0.0f, 0.5f,
+         0.5f,-0.5f
+    };
+
+    /* generate and bind buffer */
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertexPositions, GL_STATIC_DRAW); //buffer size in bytes
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        /*glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();*/
+
+        /* no index buffer defined so glDrawArrays defines index start and count */
+        /* draws bounded buffer last called by glBindBuffer() */
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* used if we have index buffer*/
+        //glDrawElements(GL_TRIANGLES, 3, NULL);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
